2137d54e2475dd13b22842cbb45f8f915ab2efaa
wgqdews <146065102+wgqdews@users.noreply.github.com>
2023-10-31 14:26:25 +0800

    Update hw5-4.c

--- hw5-4.c
+++ hw5-4.c
@@ -1,1 +1,15 @@
-
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int hou,min;
+    float ang;
+    scanf("%d%d",&hou,&min);
+    ang=hou*30-min*5.5;
+    if(ang<0)
+        ang=ang*-1;
+    if(ang>180)
+        ang=360-ang;
+    printf("%.3f",ang);
+    return 0;
+}
