62e92004807482ddc8ffb6bb780d6b25d7898da2
wgqdews <146065102+wgqdews@users.noreply.github.com>
2023-10-31 13:45:08 +0800

    Update hw5-1.c

--- hw5-1.c
+++ hw5-1.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<math.h>
+int main()
+{
+    float temC;
+    double temF,b;
+    int a;
+    scanf("%f",&temC);
+    temF=temC*9/5+32;
+    a=temF*100;
+    if(a%10>=5)
+        a=a+10;
+    printf("%d%c%d",a/100,'.',a/10%10);
 
+    return 0;
+}
