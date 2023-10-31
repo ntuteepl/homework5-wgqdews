0f7216a6a5eb442c93ff76169b92fdc757033397
wgqdews <146065102+wgqdews@users.noreply.github.com>
2023-10-31 13:54:15 +0800

    Update hw5-2.c

--- hw5-2.c
+++ hw5-2.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<math.h>
+int main()
+{
+    int min;
+    double mon;
+    scanf("%d",&min);
+    if(min>=1500)
+        mon=min*0.9*0.79;
+    else if(min>=800)
+        mon=min*0.9*0.9;
+    else
+        mon=min*0.9;
+    printf("%f",mon);
+
+    return 0;
+}
 
